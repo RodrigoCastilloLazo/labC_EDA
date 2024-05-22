ed7ff5988cb8ec6792799099d74664dade3e527c
RodrigoCLz <rcastillolaz@gmail.com>
2024-05-21 19:53:33 -0500

    Ejercicio 1 terminado

--- lab02/ejercicio01.java
+++ lab02/ejercicio01.java
@@ -4,6 +4,15 @@
 public class ejercicio01 {
 
     public static void main(String[] args) {
-        
+        System.out.println(invertNumber(12345));
+    }
+    public static int invertNumber(int n) {
+        if (Integer.toString(n).length() == 1) {
+            return n;
+        }else {
+            int aux = n % 10;
+            int large = Integer.toString(n).length();
+            return (int) Math.pow(10, large - 1) * aux + invertNumber((n - aux)/10); 
+        }
     }
 }
