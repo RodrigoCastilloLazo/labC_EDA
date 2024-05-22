8c902f1e09efa1a9d534e3da0681d0e3e0e45f8f
RodrigoCLz <rcastillolaz@gmail.com>
2024-05-21 19:59:44 -0500

    Ejercicio 2 terminado

--- lab02/ejercicio02.java
+++ lab02/ejercicio02.java
@@ -1,9 +1,30 @@
 /**
- * ejercicio01
+ * ejercicio02
  */
-public class ejercicio01 {
+public class ejercicio02 {
 
     public static void main(String[] args) {
-        
+        int numero = 54321;
+        System.out.println("El número " + numero + " está ordenado de forma creciente: " + estaOrdenadoCreciente(numero));
+        System.out.println("El número " + numero + " está ordenado de forma decreciente: " + estaOrdenadoDecreciente(numero));
+    }
+    public static boolean estaOrdenadoCreciente(int numero) {
+        String numeroStr = Integer.toString(numero);
+        for (int i = 0; i < numeroStr.length() - 1; i++) {
+            if (numeroStr.charAt(i) > numeroStr.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean estaOrdenadoDecreciente(int numero) {
+        String numeroStr = Integer.toString(numero);
+        for (int i = 0; i < numeroStr.length() - 1; i++) {
+            if (numeroStr.charAt(i) < numeroStr.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
