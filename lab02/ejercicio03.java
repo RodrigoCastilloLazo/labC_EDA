a01ba1496d31cad157b8818bc49f4a5ec9f2a4a8
RodrigoCLz <rcastillolaz@gmail.com>
2024-05-21 20:11:06 -0500

    Ejercicio 3 terminado

--- lab02/ejercicio03.java
+++ lab02/ejercicio03.java
@@ -1,9 +1,17 @@
 /**
- * ejercicio01
+ * ejercicio03
  */
-public class ejercicio01 {
+public class ejercicio03 {
 
     public static void main(String[] args) {
+        System.out.println(palindromo("srtrs"));   
+    }
+    public static boolean palindromo(String str) {
+        String aux = "";
+        for (int i = str.length(); i > 0; i--) {
+            aux = aux + str.substring(i - 1, i);
+        }
+        return aux.equals(str);
         
     }
 }
